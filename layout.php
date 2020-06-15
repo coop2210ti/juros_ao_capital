856912130a144d95b0db9f4909f9996feccf6ee8
Victor HugoJava <54379876+VICTORHUGOJS@users.noreply.github.com>
2020-06-15 11:39:08 -0300

    layout

---
+++ layout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="src/assets/layout.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <img src="src/img/logo_sicredi.png">
+        <h1 >Emissão de arquivo! - Juros ao Capital</h1>
+    </header>
+   
+<form method="POST">
+    <fieldset>  
+         <label for="data_inicial">Data Inicial </label></br>
+         <input type="text" name="data_inicial"  id="data_final" /><br></br>
+
+         <label for="data_final">Data Final </label><br>
+         <input type="text" name="data_final" id="data_final"><br></br>
+
+         <label for="data_carga">Data Carga </label><br>
+         <input type="text" name="data_carga" id="data_carga"></br></br>
+
+        <input type="button" value="Gerar arquivo" >
+    </fieldset>
+
+
+
+        
+
+
+</form>
+</body>
+</html>
